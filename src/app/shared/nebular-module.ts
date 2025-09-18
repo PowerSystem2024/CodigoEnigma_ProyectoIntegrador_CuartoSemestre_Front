78fee17ecd9200a4c3519ca42c0d27ca0c8a0c04
Lautaro Martinez <lautipro2006@gmail.com>
2025-09-17 23:29:22 -0300

    #1 Header - Se crea componente de header

---
+++ src/app/shared/nebular-module.ts
@@ -0,0 +1,45 @@
+// src/app/shared/nebular.module.ts
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  NbLayoutModule,
+  NbUserModule,
+  NbInputModule,
+  NbMenuModule,
+  NbButtonModule,
+  NbPopoverModule,
+  NbActionsModule,
+  NbCardModule,
+  NbIconModule,
+  NbSearchModule,
+} from '@nebular/theme';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NbLayoutModule,
+    NbUserModule,
+    NbInputModule,
+    NbMenuModule,
+    NbButtonModule,
+    NbPopoverModule,
+    NbActionsModule,
+    NbCardModule,
+    NbIconModule,
+    NbSearchModule,
+  ],
+  exports: [
+    CommonModule,
+    NbLayoutModule,
+    NbUserModule,
+    NbInputModule,
+    NbMenuModule,
+    NbButtonModule,
+    NbPopoverModule,
+    NbActionsModule,
+    NbCardModule,
+    NbIconModule,
+    NbSearchModule,
+  ]
+})
+export class NebularModule {}
