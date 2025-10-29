91fd1e63e9983d3bf6932072b98afe8cd6973b83
Agustin Colombo <aguscolombo16@gmail.com>
2025-10-28 21:01:27 -0300

    Agrego de carrusel de productos destacados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { RecomendadosProductsComponent } from './components/recomendados-products/recomendados-products.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { CartComponent } from './components/cart/cart.component';
     HeaderComponent,
     ProductListComponent,
     CartComponent,
+    RecomendadosProductsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
